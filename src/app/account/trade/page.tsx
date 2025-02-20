6b35349139831a44523279c1110aebe19aea1558
Ryan Bezman <ryanbezman@icloud.com>
2025-02-19 19:13:01 -0500

    this is bad

--- src/app/account/trade/page.tsx
+++ src/app/account/trade/page.tsx
@@ -43,7 +43,7 @@ export default function Trade() {
     setDisplayedBalances(newBalances);
   };
   useEffect(() => {
-    const ws = new WebSocket(`wss://${window.location.host}/ws/`);
+    const ws = new WebSocket(`wss://${window.location.host}:8443`);
     const firstBook = `${selectedBaseAsset}-${selectedQuoteAsset}`;
     ws.onopen = () => {
       ws.send(
